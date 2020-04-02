b73a3df9b444eed1a82b338d443b4ea0590d7269
Niklas <niklas.kueppers@rub.de>
2020-04-02 16:44:47 +0200

    extend: Add status and switch field to thing

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from marshmallow import Schema, fields
 from app import db, ma
 
@@ -10,22 +10,21 @@ class Room(db.Model):
     things = db.relationship('Thing', backref='location', lazy='dynamic') # pylint: disable=maybe-no-member
 
     @property
-    def things_in_room(self):
+    def total_things(self):
         return len(self.things.all())
 
 
     def __repr__(self):
         return f'<Room {self.name}>'
-
-
     
 
 class Thing(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(24), index=True, unique=True)
-    # category_id
     created_on = Column(DateTime, default=datetime.utcnow)
     room_id = Column(Integer, db.ForeignKey('room.id')) # pylint: disable=maybe-no-member
+    status = Column(Boolean, default=False)
+    last_switched = Column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<Thing {self.name}>'
@@ -35,6 +34,7 @@ class RoomSchema(Schema):
     id = fields.Int()
     name = fields.String()
     created_on = fields.DateTime()
+    total_things = fields.Int()
     things = fields.Nested('ThingSchema', many=True, only=['id', 'name'])
     
 
@@ -43,3 +43,5 @@ class ThingSchema(Schema):
     name = fields.String()
     created_on = fields.DateTime()
     location = fields.Nested(RoomSchema, only=['id', 'name'])
+    status = fields.Boolean()
+    last_switched = fields.DateTime()
