3417e2ad00e43484db95e75822a24ac8bc6b2485
Niklas <niklas.kueppers@rub.de>
2020-04-08 22:18:43 +0200

    extend: readme

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,9 +7,7 @@ from app.main import bp
 
 @bp.route('/')
 def home():
-    
     response = requests.get(request.url_root + '/api/rooms')
     rooms = response.json()['result']
-    
 
     return render_template('main/home.html', rooms=rooms)
