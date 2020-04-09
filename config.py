fbc27d694baf8e58c75490044ca91629f387341d
Niklas <niklas.kueppers@rub.de>
2020-04-09 16:16:17 +0200

    extend: Configure for deployment

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'MAetapAEjDx8S2wmbEgKKVTCVZFgx6eupExY4CxZ'
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'MAetapAEjDx8S2wmbEgKKVTCVZFgx6eupExY4CxZ'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
     #print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
